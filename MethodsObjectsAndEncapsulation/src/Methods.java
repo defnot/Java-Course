cd44fe092dfc0d2b1be23761aec186ded9813687
Dany <dimitrov829@gmail.com>
2016-11-10 16:59:02 +0200

    first commit

---
+++ MethodsObjectsAndEncapsulation/src/Methods.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Dany on 11/10/2016.
+ */
+public class Methods {
+
+    public static void main(String[] args) {
+        System.out.println(new Methods());
+
+    }
+
+    @Override
+    public String toString() {
+        return "adsfasdf";
+    }
+
+}
